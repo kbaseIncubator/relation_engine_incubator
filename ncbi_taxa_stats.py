deb431b1735510e4ae03a6b2c2dbf342cc08cdfc
Gavin <gaprice@lbl.gov>
2019-07-18 17:49:10 -0700

    Bugfix, histogram
    node child count was leaving out childless nodes

--- ncbi_taxa_stats.py
+++ ncbi_taxa_stats.py
@@ -9,11 +9,14 @@
 
 # Arguments - the directory containing the uncompressed NCBI taxon data.
 
+import argparse
+from collections import defaultdict
 import os
 import re
 import sys
+import numpy as np
 import statistics as stats
-from collections import defaultdict
+import texttable
 
 NAMES_FILE = 'names.dmp'
 NODES_FILE = 'nodes.dmp'
@@ -28,26 +31,55 @@ def calculate_stats(path, feature_index):
         last_tax = -1
         for l in f:
             l = re.split(SEP, l)
-            tax_id = int(l[TAXID_IDX])
+            tax_id_str = l[TAXID_IDX].strip()
+            tax_id = int(tax_id_str)
+            d[tax_id_str] # init to 0 if not already in dict
             if last_tax > tax_id:
                 raise ValueError('Found unsorted file {} at tax id {}'.format(path, tax_id))
             last_tax = tax_id
-            d[l[feature_index]] += 1
+            d[l[feature_index].strip()] += 1
     # crappy efficiency here but meh
     mean = stats.mean(d.values())
-    return mean, stats.median(d.values()), max(d.values()), stats.pstdev(d.values(), mean)
+    mx = max(d.values())
+    hist_bins = []
+    bn = 1
+    while bn < mx:
+        bn = bn * 10
+        hist_bins.append(bn)
+    hist_bins = [0, 1, 2, 3] + hist_bins
+    hist = np.histogram(list(d.values()), hist_bins)[0]
+    hist_bins.remove(3)
+    return {'mean': mean,
+            'median': stats.median(d.values()),
+            'max': mx,
+            'stddev': stats.pstdev(d.values(), mean),
+            'hist': hist,
+            'hist_bins': hist_bins
+            }
 
-def print_stats(datatype, mean, median, max, stddev):
+def print_stats(datatype, mean, median, max, stddev, hist, hist_bins):
     print('{}: Mean: {} Median: {} Max: {} Stddev: {}'.format(datatype, mean, median, max, stddev))
+    tt = texttable.Texttable()
+    tt.set_deco(0)
+    tt.add_row([datatype, 'Node count'])
+    for h, b in zip(hist, hist_bins):
+        tt.add_row([b, h])
+    print(tt.draw())
+    print()
+
+
+def parseargs():
+    parser = argparse.ArgumentParser(description='Calculate statistics on an NCBI taxonomy dump.')
+    parser.add_argument('--dir', required=True, help='the directory containing the dump file')
+
+    return parser.parse_args()
 
 def main():
-    if len(sys.argv) < 2:
-        sys.exit('Must specify directory with NCBI taxa files')
-    directory = sys.argv[1]
+    a = parseargs()
 
-    print_stats('Names per taxa', *calculate_stats(os.path.join(directory, NAMES_FILE), TAXID_IDX))
+    print_stats('Names per taxa', **calculate_stats(os.path.join(a.dir, NAMES_FILE), TAXID_IDX))
     print_stats('Children per taxa',
-        *calculate_stats(os.path.join(directory, NODES_FILE), NODES_PARENT_IDX))
+        **calculate_stats(os.path.join(a.dir, NODES_FILE), NODES_PARENT_IDX))
 
 if __name__ == '__main__':
     main()
