b69c7fdb9bb5d95117c420c8006bf4d0c0ded837
Gavin <gaprice@lbl.gov>
2019-07-25 17:54:32 -0700

    Calculate parent & node changes
    Node changes now don't take parent or fields we don't use into account & do
    take names into account

--- ncbi/ncbi_taxa_node_transitions.py
+++ ncbi/ncbi_taxa_node_transitions.py
@@ -6,6 +6,7 @@
 
 import argparse
 import hashlib
+import json
 import pathlib
 import re
 import texttable
@@ -17,6 +18,7 @@ ST_DELETE = 'D'
 ST_MERGED = 'M'
 ARROW = '→'
 
+NAMES_IN_FILE = 'names.dmp'
 NODES_IN_FILE = 'nodes.dmp'
 DEL_IN_FILE = 'delnodes.dmp'
 MERGED_IN_FILE = 'merged.dmp'
@@ -27,6 +29,7 @@ MERGED = 'merged'
 LINE_MD5S = 'MD5S'
 
 SEP = r'\s\|\s?'
+SCI_NAME = 'scientific name'
 
 KEY_TO_FILE = {NODES: NODES_IN_FILE,
                DELETED: DEL_IN_FILE,
@@ -38,17 +41,62 @@ KEY_TO_STATE = {NODES: ST_EXIST,
                 MERGED: ST_MERGED
                 }
 
+# TODO Update this to use common code when it works
+
+def load_names(name_file):
+    # Could make this use less memory by parsing one nodes worth of entries at a time, since
+    # both the names and nodes files are sorted by taxid. YAGNI for now
+    name_table = defaultdict(lambda: defaultdict(list))
+    with open(name_file) as nf:
+        for line in nf:
+            tax_id, name, _, category = re.split(SEP, line)[0:4]
+            name_table[tax_id.strip()][category.strip()].append(name.strip())
+
+    return {k: dict(name_table[k]) for k in name_table.keys()}
+
+def make_node_md5(node_line, names):
+    # should really make the ints constants but meh
+    id_, rank, gencode = [node_line[i].strip() for i in [0,2,6]]
+
+    aliases = []
+    # May need to move names into separate nodes for canonical search purposes
+    for cat in list(names[id_].keys()):
+        if cat != SCI_NAME:
+            for nam in names[id_][cat]:
+                aliases.append({'category':  cat, 
+                                'name':      nam, 
+                                })
+
+    # vertex
+    sci_names = names[id_][SCI_NAME]
+    if len(sci_names) != 1:
+        raise ValueError('Node {} has {} scientific names'.format(id_, len(sci_names)))
+    node = {
+            'id':                         id_,
+            'scientific_name':            sci_names[0],
+            'rank':                       rank,
+            'aliases':                    aliases,
+            'ncbi_taxon_id':              int(id_),
+            'gencode':                    gencode,
+            }
+    jstr = json.dumps(node, sort_keys=True)
+    return hashlib.md5(jstr.encode('UTF-8')).hexdigest()
+
 def load_nodestates(taxdir):
+    names = load_names(taxdir / NAMES_IN_FILE)
+    parents = {}
     ret = defaultdict(set)
     md5s = dict()
     for key, f in KEY_TO_FILE.items():
         with open(taxdir / f) as taxfile:
             for l in taxfile:
-                id_ = re.split(SEP, l)[0].strip()
+                record = re.split(SEP, l)
+                id_ = record[0].strip()
                 ret[key].add(id_)
                 if key == NODES:
-                    md5s[id_] = hashlib.md5(l.encode('UTF-8')).hexdigest()
-    return dict(ret), md5s
+                    parents[id_] = record[1].strip()
+                    md5s[id_] = make_node_md5(record, names)
+    return dict(ret), md5s, parents
 
 def get_state(nodeid, nodestates):
     for key, state in KEY_TO_STATE.items():
@@ -75,19 +123,25 @@ def main():
     transitions = defaultdict(int)
     # last here means last iteration, not last in list
     last_dir = dirs.pop(0)
-    last_nodestates, last_md5s = load_nodestates(p / last_dir)
+    last_nodestates, last_md5s, last_parents = load_nodestates(p / last_dir)
     sizes = {last_dir: {'n': len(last_nodestates[NODES]),
                         'd': len(last_nodestates[DELETED]),
                         'm': len(last_nodestates[MERGED])
                         }
              }
     mismatches = {}
+    parent_changes = {}
     for d in dirs:
-        nodestates, md5s = load_nodestates(p / d)
+        print(f'Processing {d}')
+        nodestates, md5s, parents = load_nodestates(p / d)
         mismatches[d] = 0
         for id_ in md5s.keys():
             if id_ in last_md5s and last_md5s[id_] != md5s[id_]:
                 mismatches[d] += 1
+        parent_changes[d] = 0
+        for id_ in parents.keys():
+            if id_ in last_parents and last_parents[id_] != parents[id_]:
+                parent_changes[d] += 1
         sizes[d] = {'n': len(nodestates[NODES]),
                     'd': len(nodestates[DELETED]),
                     'm': len(nodestates[MERGED])
@@ -98,6 +152,7 @@ def main():
                 transitions[laststate + ARROW + state] += 1
         last_nodestates = nodestates
         last_md5s = md5s
+        last_parents = parents
     
     s = sum(transitions.values())
 
@@ -122,6 +177,14 @@ def main():
     tt.add_row(['Tax dump', 'Node changes'])
     for k in sorted(mismatches.keys()):
         tt.add_row([k, mismatches[k]])
+    print(tt.draw())
+    print()
+
+    tt = texttable.Texttable()
+    tt.set_deco(0)
+    tt.add_row(['Tax dump', 'Node parent changes'])
+    for k in sorted(parent_changes.keys()):
+        tt.add_row([k, parent_changes[k]])
     print(tt.draw())
 
 if __name__ == '__main__':
