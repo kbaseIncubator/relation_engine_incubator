8e86735771bf4b73cfb14b819f6cf8c319bfee7c
Gavin <gaprice@lbl.gov>
2019-07-30 15:30:15 -0700

    save a vertex

--- relation_engine/database/test/batch_time_travel_test.py
+++ relation_engine/database/test/batch_time_travel_test.py
@@ -26,39 +26,60 @@ def arango_db():
 
 def test_get_vertex(arango_db):
     col_name = 'verts'
-    arango_db.create_collection(col_name)
-    col = arango_db.collection(col_name)
+    col = arango_db.create_collection(col_name)
 
     # it's assumed that given an id and a timestamp there's <= 1 match in the collection
-    col.import_bulk([{'_key': '1', 'id': 'foo', 'created': 100, 'expires': 600},
-                     {'_key': '2', 'id': 'bar', 'created': 100, 'expires': 200},
-                     {'_key': '3', 'id': 'bar', 'created': 201, 'expires': 300}, # target
-                     {'_key': '4', 'id': 'bar', 'created': 301, 'expires': 400},
+    col.import_bulk([{'_key': '1', 'id': 'foo', 'created': 100, 'expired': 600},
+                     {'_key': '2', 'id': 'bar', 'created': 100, 'expired': 200},
+                     {'_key': '3', 'id': 'bar', 'created': 201, 'expired': 300}, # target
+                     {'_key': '4', 'id': 'bar', 'created': 301, 'expired': 400},
                      ])
 
     att = ArangoBatchTimeTravellingDB(arango_db, default_vertex_collection=col_name)
-    ret = att.get_vertex('bar', 201)
 
-    assert ret == {'_key': '3', 'id': 'bar', 'created': 201, 'expires': 300}
+    ret = att.get_vertex('bar', 201)
+    assert ret == {'_key': '3', 'id': 'bar', 'created': 201, 'expired': 300}
 
     ret = att.get_vertex('bar', 300)
-
-    assert ret == {'_key': '3', 'id': 'bar', 'created': 201, 'expires': 300}
+    assert ret == {'_key': '3', 'id': 'bar', 'created': 201, 'expired': 300}
 
     ret = att.get_vertex('foo', 250)
-
-    assert ret == {'_key': '1', 'id': 'foo', 'created': 100, 'expires': 600}
+    assert ret == {'_key': '1', 'id': 'foo', 'created': 100, 'expired': 600}
 
     assert att.get_vertex('bar', 99) == None
     assert att.get_vertex('bar', 401) == None
 
-    col.insert({'_key': '5', 'id': 'bar', 'created': 150, 'expires': 250})
+    col.insert({'_key': '5', 'id': 'bar', 'created': 150, 'expired': 250})
 
     try:
         att.get_vertex('bar', 200)
     except ValueError as e:
         assert e.args[0] == 'db contains > 1 vertex for id bar, timestamp 200, collection verts'
 
+def test_save_vertex(arango_db):
+    col_name = 'verts'
+    arango_db.create_collection(col_name)
+    att = ArangoBatchTimeTravellingDB(arango_db, default_vertex_collection=col_name)
 
+    att.save_vertex('myid', 'load-ver1', 500, {'science': 'yes!'})
+    att.save_vertex('myid2', 'load-ver1', 600, {'science': 'yes indeed!'})
+
+    ret = att.get_vertex('myid', 600)
+    assert ret == {'_key': 'myid_load-ver1',
+                   'created': 500,
+                   'expired': 9007199254740991,
+                   'first_version': 'load-ver1',
+                   'id': 'myid',
+                   'last_version': 'load-ver1',
+                   'science': 'yes!'}
+    
+    ret = att.get_vertex('myid2', 600)
+    assert ret == {'_key': 'myid2_load-ver1',
+                   'created': 600,
+                   'expired': 9007199254740991,
+                   'first_version': 'load-ver1',
+                   'id': 'myid2',
+                   'last_version': 'load-ver1',
+                   'science': 'yes indeed!'}
 
 
