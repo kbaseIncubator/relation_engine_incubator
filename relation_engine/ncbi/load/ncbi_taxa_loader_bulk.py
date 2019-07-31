e1b351651be35526a77d5be372e44dad49580a01
Gavin <gaprice@lbl.gov>
2019-07-31 14:18:43 -0700

    Move common load code into common ares

--- relation_engine/ncbi/load/ncbi_taxa_loader_bulk.py
+++ relation_engine/ncbi/load/ncbi_taxa_loader_bulk.py
@@ -23,15 +23,11 @@
 # TODO TESTS
 
 import argparse
-import json
 import os
-import re
-import sys
 import unicodedata
-from collections import defaultdict
-from pprint import pprint
 
-# probably want to namespace this behind biokbase
+from relation_engine.load_utils import process_nodes
+from relation_engine.load_utils import process_edges
 from relation_engine.ncbi.taxa_parsers import NCBINodeProvider
 from relation_engine.ncbi.taxa_parsers import NCBIEdgeProvider
 
@@ -40,66 +36,6 @@ EDGES_OUT_FILE = 'ncbi_taxa_edges.json'
 
 NAMES_IN_FILE = 'names.dmp'
 NODES_IN_FILE = 'nodes.dmp'
-
-# see https://www.arangodb.com/2018/07/time-traveling-with-graph-databases/
-# in unix epoch ms this is 2255/6/5
-MAX_ADB_INTEGER = 2**53 - 1
-
-def process_nodes(nodeprov, load_version, timestamp, nodes_out):
-    """
-    Process graph nodes from a provider into a JSON load file for a batch time travelling load.
-
-    This function is only suitable for the initial load in the time travelling database.
-    Further loads must use a delta load algorithm.
-
-    Nodes are expected to have an 'id' field containing the node's unique ID.
-
-    nodeprov - the node provider. This is an iterable that returns nodes represented as dicts.
-    load_version - the version of the load in which the nodes appear.
-    timestamp - the timestamp at which the nodes will begin to exist.
-    nodes_out - a handle to the file where the nodes will be written.
-    """
-    for n in nodeprov:
-        n.update({
-            '_key':           n['id'] + '_' + load_version,
-            'first_version':  load_version,
-            'last_version':   load_version,
-            'created':        timestamp,
-            'expires':        MAX_ADB_INTEGER
-            })
-        nodes_out.write(json.dumps(n) + '\n')
-
-def process_edges(edgeprov, node_collection, load_version, timestamp, edges_out):
-    """
-    Process graph edges from a provider into a JSON load file for a batch time travelling load.
-
-    This function is only suitable for the initial load in the time travelling database.
-    Further loads must use a delta load algorithm.
-
-    Edges are expected to have the following fields:
-    id - the edge's unique ID.
-    from - the unique ID of the vertex from where the edge originates.
-    to - the unique ID of the vertex where the edge terminates.
-
-    edgeprov - the edge provider. This is an iterable that returns edges represented as dicts.
-    node_collection - the name of the collection in which the nodes associated with the edges
-      reside. This is used to generate the _from and _to fields.
-    load_version - the version of the load in which the edges appear.
-    timestamp - the timestamp at which the edges will begin to exist.
-    edges_out - a handle to the file where the edges will be written.
-    
-    """
-    for e in edgeprov:
-        e.update({
-            '_key':             e['id'] + '_' + load_version,
-            '_from':            node_collection + '/' + e['from'] + '_' + load_version,
-            '_to':              node_collection + '/' + e['to'] + '_' + load_version,
-            'first_version':    load_version,
-            'last_version':     load_version,
-            'created':          timestamp,
-            'expires':          MAX_ADB_INTEGER,
-        })
-        edges_out.write(json.dumps(e) + '\n')
 
 def parse_args():
     parser = argparse.ArgumentParser(
